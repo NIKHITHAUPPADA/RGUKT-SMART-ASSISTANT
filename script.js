2932f6a323ddd9fa06532d21d127f6bfce62fe5d
NIKHITHAUPPADA <nikhithakanna036@gmail.com>
2025-11-07 14:55:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let userName = prompt("Welcome! Please enter your name:");
+
+if (userName) {
+    alert("Hello, " + userName + "! Welcome to RGUKT Smart Assistant.");
+} else {
+    alert("Hello! Welcome to RGUKT Smart Assistant.");
+}
